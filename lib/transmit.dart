a2138b544aed108d16e220cce945bcb5ef7fc8a0
Robert McDermot <robert.mcdermot@gmail.com>
2016-01-17 09:39:56 -0500

    allow for easier single channel service listeners

--- lib/transmit.dart
+++ lib/transmit.dart
@@ -1,13 +1,16 @@
 library transmit;
-import 'dart:async';
+
 import 'dart:html';
 import 'dart:js';
 
 /// fire a message and trigger activated services.
 transmit(var channel, [var content]) {
-  if (!(channel is String) && !(channel is int)) throw('channel must be a String or and int');
-  var event = new CustomEvent('PUMP_' + channel, detail: content);
-  document.dispatchEvent(event);
+	if (!(channel is String) && !(channel is int)) {
+		throw('channel must be a String or and int');
+	}
+
+	var event = new CustomEvent('PUMP_' + channel, detail: content);
+	document.dispatchEvent(event);
 }
 
 /// A [JsFunction] that calls 'transmit'.
@@ -16,13 +19,27 @@ JsFunction get jsTransmit => context['transmit'];
 
 /// A [Service] reacts to every message transmitted of type in [types]
 class Service {
-  Function callback;
+	Function callback;
 
-  Service(final List channels, this.callback) {
-    for (var channel in channels) {
-      if (!(channel is String) && !(channel is int)) throw('channel must be a String or and int');
-      document.addEventListener('PUMP_' + channel.toString(),
-          (CustomEvent event) => callback(event.detail));
-    }
-  }
+	///[channelOrListOfChannels] must either be a List<String> of channels or
+	///a single channel name as a String
+	factory Service(dynamic channelOrListOfChannels, Function callback) {
+		assert(channelOrListOfChannels is List<String> || channelOrListOfChannels is String);
+
+		if (channelOrListOfChannels is String) {
+			return new Service._([channelOrListOfChannels], callback);
+		} else {
+			return new Service._(channelOrListOfChannels, callback);
+		}
+	}
+
+	Service._(final List channels, this.callback) {
+		for (var channel in channels) {
+			if (!(channel is String) && !(channel is int)) {
+				throw('channel must be a String or and int');
+			}
+			document.addEventListener('PUMP_' + channel.toString(),
+				                          (CustomEvent event) => callback(event.detail));
+		}
+	}
 }
